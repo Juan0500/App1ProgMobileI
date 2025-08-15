4728a6a0a56dcc7c8dac3571d23cbeacb4e71b52
Juan Carballo <emanuelcarballo2004@gmail.com>
2025-08-15 20:53:48 -0300

    Segunda clase

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,32 @@ class aplicativo extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Meu App"),
+          title: Text("Meu App", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
           centerTitle:true,
           backgroundColor: Colors.amber,
           foregroundColor: Colors.white,
         ),
-        body: Text("Conteudo atual do App"),
+        // body: Center(child: Text("Conteudo atual do App", style: TextStyle(fontSize: 18),))
+        // body: ElevatedButton(onPressed: (){}, child: Text("Salvar")),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Text("Nota 1"),
+                  Text("Nota 2"),
+                  Text("Nota 3")
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Text("Conteudo", style: TextStyle(fontSize: 30),),
+              ElevatedButton(onPressed: (){}, child: Text("Salvar")),
+            ],
+          ),
+        ),
       ),
     );
   }
